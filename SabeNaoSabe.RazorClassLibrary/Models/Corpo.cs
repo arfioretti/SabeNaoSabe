84f847b3ddee4270bb2c56b92253c76837a10f23
aristides ricardo fioretti <arfioretti@gmail.com>
2024-06-28 12:27:52 -0300

    questionario respondendo ok

--- SabeNaoSabe.RazorClassLibrary/Models/Corpo.cs
+++ SabeNaoSabe.RazorClassLibrary/Models/Corpo.cs
@@ -8,14 +8,19 @@ namespace SabeNaoSabe.RazorClassLibrary.Models
 {
     public class Corpo
     {
+        public int Id { get; set; }
         public int Tipo { get; set; }
         public string Pergunta { get; set; }
         public List<Opcao> Opcoes { get; set; }
-        public string Resposta { get; set; }
+        public int Resposta { get; set; }
+        public List<int> Gabaritos { get; set; }
         public string Explicacao { get; set; }
+        public string Resultado { get; set; }
     }
     public class Opcao
-    {
+    { 
+        public int Id { get; set; }
+        public bool Checked { get; set; }
         public string Descricao { get; set; }
     }
 }
